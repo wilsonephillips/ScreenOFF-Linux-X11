12dd947a7343664c894b54b7510ce3f10fd2a077
Wilson Phillips <WilsonEPhillips@hotmail.com>
2023-06-23 07:57:32 -0500

    Update ScreenOff.sh

--- ScreenOff.sh
+++ ScreenOff.sh
@@ -3,4 +3,4 @@
 # This script will immediately put the monitor into powersave mode.
 # Any mouse movement or keystroke will power the monitor on again.
 
-sleep 1; xset dpms force off
+sleep 3; xset dpms force off
