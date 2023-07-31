822880e722b666b1f79039582a23e49c8b5ec9f5
Wilson Phillips <WilsonEPhillips@hotmail.com>
2023-07-31 11:03:26 -0500

    Update ScreenOff.sh

--- ScreenOff.sh
+++ ScreenOff.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+# puts the monitor into powersave mode.
+# Any mouse movement or keystroke will turn it on again.
 
-# This script will immediately put the monitor into powersave mode.
-# Any mouse movement or keystroke will power the monitor on again.
+# It needs to be placed in a location in the $PATH and then drop a link
+# to it on the desktop. Don't forget to set it to executable!
 
-sleep 3; xset dpms force off
+sleep 3  # Gives 3 seconds to let go of the mouse if clicking a link on the desktop.
+
+# Choose the one that works for you.
+#xset dpms force suspend
+xset dpms force standby
+#xset dpms force off
